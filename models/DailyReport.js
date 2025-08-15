a40ce37cac60efd916a74deb98eed945f7594da1
Fevzi Atanoğlu <95905332+fevziatanoglu@users.noreply.github.com>
2025-08-15 18:13:01 +0300

    feat(dailyReportModel): create DailyReport model with schema for sales and customer tracking

---
+++ models/DailyReport.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const dailyReportSchema = new mongoose.Schema({
+    adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    date: { type: Date, required: true },
+    totalSales: { type: Number, required: true },
+    totalCustomers: { type: Number, required: true },
+    items: [{
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+        productName: { type: String, required: true },
+        price: { type: Number, required: true },
+        quantity: { type: Number, required: true, default: 1 }
+    }
+    ],
+});
+
+const DailyReport = mongoose.model('DailyReport', dailyReportSchema);
+
+export default DailyReport;
